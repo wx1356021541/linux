40905fe7f39e3014f627c980a75679a0412b2840
wx1356021541 <67359393+wx1356021541@users.noreply.github.com>
2020-06-26 13:24:57 +0800

    Update sh3pro.c

--- sh3pro.c
+++ sh3pro.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
-int main()
+int mysys( char* command)
+{
+    pid_t pid;
+    int status;
+
+    if(command == NULL)
+    {
+        return (1);
+    }
+    if((pid = fork())<0)
+    {
+    	status = -1;
+    }
+    else if(pid == 0)
+    {
+        execl("/bin/sh", "sh", "-c", command, (char *)0);
+        exit(11);
+    }
+    else
+    {
+        wait( &status);
+    }
+    return status;
+}
+
+int mypipe(char *str1,char *str2)
 {
     int pid;    
     int fd[2];
@@ -15,7 +47,7 @@ int main()
         close(fd[0]);
         close(fd[1]);
 
-        execlp("cat", "cat", "/etc/passwd", NULL);
+        execl("/bin/sh", "sh", "-c", str1, (char *)0);
         exit(0);
     }
     // parent
@@ -24,7 +56,41 @@ int main()
     close(fd[1]);
 
     read(0, buf, sizeof(buf)); 
-    execlp("wc", "wc", "-l", NULL);
+    execl("/bin/sh", "sh", "-c", str2, (char *)0);
     printf("Receive:%s\n", buf); 
+    return 0;
+}
+int main()
+{
+    char str[1000];
+	char *p,*mingling1,*mingling2;
+	int i;
+    while(1)
+    {
+        printf(">");
+        gets(str);
+		mingling1 = str;
+		mingling2 = str;
+
+        if(strcmp(str,"exit") == 0)
+            break;
+
+		for(p=str;p!='\0';p++)
+		{
+			if(*p == '|')
+			{
+				mingling1[p-str]='\0';
+				mingling2 = p+1;
+				break;
+			}
+		}
+		if(*p == '|')
+		{
+			mypipe(mingling1,mingling2);
+		}
+		else 
+			mysys(str);
+    }
+
     return 0;
 }
